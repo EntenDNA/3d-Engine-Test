a5236bc722685f78005a1bf9975d97444f5ee402
BuildTools <unconfigured@null.spigotmc.org>
2020-12-22 14:32:16 +0100

    Added rotating cube as test

--- src/me/Nanook/main/Game.java
+++ src/me/Nanook/main/Game.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.*;
 
 import me.Nanook.util.mesh;
 import me.Nanook.util.triangle;
@@ -54,19 +53,32 @@ public class Game implements Runnable{
     private double fNear = 0.1f;
     private double fFar = 1000;
     private double fFov = 90;
-    private double fAspectRatio = HEIGHT/WIDTH;
-	private double fFovRad = 1 / Math.tan(fFov * 0.5 / 180 * 3.1415);
+    private double fAspectRatio;
+	private double fFovRad;
+	private double fTheta;
+	private double offset;
     
-    private double[][] matProj = new double[][] {{fAspectRatio * fFovRad, 0, 0, 0},
-											     {0, fFovRad, 0, 0},
-											     {0, 0, fFar / (fFar - fNear), 1},
-											     {0, 0, (-fFar * fNear) / (fFar - fNear), 0}};
+    private double[][] matProj;
+    private double[][] matRotX;
+    private double[][] matRotY;
+    private double[][] matRotZ;
 	
 	public Game(String title, int width, int height)
 	{
 		this.HEIGHT = height;
 		this.WIDTH = width;
 		this.TITLE = title;
+		
+		this.fAspectRatio = HEIGHT/WIDTH;
+		this.fFovRad = 1 / Math.tan(fFov * 0.5 / 180 * 3.1415);
+	    
+		this.matProj = new double[][] {{fAspectRatio * fFovRad, 0, 0, 0},
+							     		{0, fFovRad, 0, 0},
+							     		{0, 0, fFar / (fFar - fNear), 1},
+							     		{0, 0, (-fFar * fNear) / (fFar - fNear), 0}};
+							     				
+		this.fTheta = 0;
+		this.offset = 3;
 	}
 	
 	private void init()
@@ -88,41 +100,73 @@ public class Game implements Runnable{
 		// clear screen
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 		
+		// calculate rotation matrices
+		fTheta += 0.0002;
+		double[][] matRotZ = {{Math.cos(fTheta), -Math.sin(fTheta), 0, 0},
+				   			  {Math.sin(fTheta), Math.cos(fTheta), 0, 0},
+				   			  {0, 0, 1, 0},
+				   			  {0, 0, 0, 1}};
+								
+		double[][] matRotY = {{Math.cos(fTheta*0.3), 0, Math.sin(fTheta*0.3), 0},
+	   			  			  {0, 1, 0, 0},
+	   			  			  {-Math.sin(fTheta*0.3), 0, Math.cos(fTheta*0.3), 0},
+	   			  			  {0, 0, 0, 1}};
+							
+		double[][] matRotX = {{1, 0, 0, 0},
+		  			  		  {0, Math.cos(fTheta *0.5), -Math.sin(fTheta *0.5), 0},
+		  			  		  {0, Math.sin(fTheta *0.5), Math.cos(fTheta *0.5), 0},
+		  			  		  {0, 0, 0, 1}};
+					
 		for (triangle tri : meshCube.tris)
 		{
-			triangle triProjected = new triangle(MultiplyMartixVector(tri.vec3dList[0], matProj),
-												MultiplyMartixVector(tri.vec3dList[1], matProj),
-												MultiplyMartixVector(tri.vec3dList[2], matProj));
+			triangle triProjected, triTranslated, triRotatedX, triRotatedY, triRotatedZ;
 			
-			g.drawPolygon(new int[] {triProjected.vec3dList[0].x, triProjected.vec3dList[1].x, triProjected.vec3dList[2].x},
-						  new int[] {triProjected.vec3dList[0].y, triProjected.vec3dList[1].y, triProjected.vec3dList[2].y}, 3);
+			// rotation x
+			triRotatedX = new triangle(MultiplyMartixVector(tri.vec3dList[0], matRotX),
+										MultiplyMartixVector(tri.vec3dList[1], matRotX),
+										MultiplyMartixVector(tri.vec3dList[2], matRotX));
+			
+			// rotation y
+			triRotatedY = new triangle(MultiplyMartixVector(triRotatedX.vec3dList[0], matRotY),
+										MultiplyMartixVector(triRotatedX.vec3dList[1], matRotY),
+										MultiplyMartixVector(triRotatedX.vec3dList[2], matRotY));
+			
+			// rotation z
+			triRotatedZ = new triangle(MultiplyMartixVector(triRotatedX.vec3dList[0], matRotZ),
+										MultiplyMartixVector(triRotatedX.vec3dList[1], matRotZ),
+										MultiplyMartixVector(triRotatedX.vec3dList[2], matRotZ));
+			
+			// translate triangle
+			triTranslated = triRotatedZ.deepCopy();
+			triTranslated.vec3dList[0].z += offset;
+			triTranslated.vec3dList[1].z += offset;
+			triTranslated.vec3dList[2].z += offset;
+			
+			// project onto screen
+			triProjected = new triangle(MultiplyMartixVector(triTranslated.vec3dList[0], matProj),
+										MultiplyMartixVector(triTranslated.vec3dList[1], matProj),
+										MultiplyMartixVector(triTranslated.vec3dList[2], matProj));
+			
+			// scale into view
+			triProjected.vec3dList[0].x += 1.0; triProjected.vec3dList[0].y += 1.0;
+			triProjected.vec3dList[1].x += 1.0; triProjected.vec3dList[1].y += 1.0;
+			triProjected.vec3dList[2].x += 1.0; triProjected.vec3dList[2].y += 1.0;
+			triProjected.vec3dList[0].x *= 0.5 * this.WIDTH;
+			triProjected.vec3dList[0].y *= 0.5 * this.HEIGHT;
+			triProjected.vec3dList[1].x *= 0.5 * this.WIDTH;
+			triProjected.vec3dList[1].y *= 0.5 * this.HEIGHT;
+			triProjected.vec3dList[2].x *= 0.5 * this.WIDTH;
+			triProjected.vec3dList[2].y *= 0.5 * this.HEIGHT;
+			
+			// draw triangle
+			g.drawPolygon(new int[] {(int) triProjected.vec3dList[0].x, (int) triProjected.vec3dList[1].x, (int) triProjected.vec3dList[2].x},
+						  new int[] {(int) triProjected.vec3dList[0].y, (int) triProjected.vec3dList[1].y, (int) triProjected.vec3dList[2].y}, 3);
 		}
 		
 		// end drawing
 		bs.show();
 		g.dispose();
 		
-	}
-	
-	private void render()
-	{
-		bs = display.getCanvas().getBufferStrategy();
-		if(bs == null)
-		{
-			display.getCanvas().createBufferStrategy(3);
-			return;
-		}
-		
-		g = bs.getDrawGraphics();
-		
-		// clear screen
-		g.clearRect(0, 0, WIDTH, HEIGHT);
-		
-		// start drawing
-		
-		// end drawing
-		bs.show();
-		g.dispose();
 	}
 	
 	@Override
@@ -133,7 +177,6 @@ public class Game implements Runnable{
 		while(running)
 		{
 			tick();
-			render();
 		}
 		
 		stop();
--- src/me/Nanook/util/triangle.java
+++ src/me/Nanook/util/triangle.java
@@ -1,6 +1,6 @@
 package me.Nanook.util;
 
-public class triangle {
+public class triangle{
 	
 	public vec3d[] vec3dList = new vec3d[3];
 	
@@ -9,5 +9,12 @@ public class triangle {
 		this.vec3dList[0] = v1;
 		this.vec3dList[1] = v2;
 		this.vec3dList[2] = v3;
+	}
+	
+	public triangle deepCopy()
+	{
+		return new triangle(new vec3d(this.vec3dList[0].x, this.vec3dList[0].y, this.vec3dList[0].z),
+							new vec3d(this.vec3dList[1].x, this.vec3dList[1].y, this.vec3dList[1].z),
+							new vec3d(this.vec3dList[2].x, this.vec3dList[2].y, this.vec3dList[2].z));
 	}
 }
